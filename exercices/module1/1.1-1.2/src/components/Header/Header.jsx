767dcb353b6b5af7a6f0f2e14d66379152caa4aa
PADebin <pierre.debin@student.vinci.be>
2024-09-17 16:56:52 +0200

    1.1-1.2 : import image

--- exercices/module1/1.1-1.2/src/components/Header/Header.jsx
+++ exercices/module1/1.1-1.2/src/components/Header/Header.jsx
@@ -1,6 +1,13 @@
 /* eslint-disable react/prop-types */
+import Image from "components/Header/LOGO HE VINCI.png";
+
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return (
+    <>
+      <img src={Image} alt="Vinci logo" />
+      <h1>{props.course}</h1>
+    </>
+  );
 };
 
 export default Header;
