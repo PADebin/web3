63c0bb4bc96d7cdda37c197c9dc88907d7b59bc0
PADebin <pierre.debin@student.vinci.be>
2025-08-12 17:56:37 +0200

    new:ex2.1-init

---
+++ exercices/module2/2.1/src/components/ClickCounter/ClickCounter.jsx
@@ -0,0 +1,14 @@
+function ClickCounter({ count, setCount }) {
+  return (
+    <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
+    </div>
+  );
+}
+
+export default ClickCounter;
--- exercices/module2/start-state/src/components/Main/DrinkCard.jsx
+++ exercices/module2/start-state/src/components/Main/DrinkCard.jsx
@@ -1,14 +1,9 @@
-const DrinkCard = (props) => {
+const DrinkCard = ({ title, image, children }) => {
   return (
     <div className="drink-card">
-      <img
-        src={props.image}
-        alt={props.title}
-        className="drink-image"
-        width="50"
-      />
-      <h2>{props.title}</h2>
-      <div className="drink-details">{props.children}</div>
+      <img src={image} alt={title} className="drink-image" width="50" />
+      <h2>{title}</h2>
+      <div className="drink-details">{children}</div>
     </div>
   );
 };
