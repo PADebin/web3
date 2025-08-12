2711bc89e8c07d84098919d506e23e46ff9a39ff
PADebin <pierre.debin@student.vinci.be>
2025-08-12 16:39:58 +0200

    1.1-1.2 : import image

--- exercices/module1/1.1-1.2/src/components/Header/Header.jsx
+++ exercices/module1/1.1-1.2/src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
+import logo from "./Moovin.png";
+
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return (
+    <div>
+      <img src={logo} alt="Moovin Logo" />
+      <h1>{props.course}</h1>
+    </div>
+  );
 };
 
 export default Header;
--- exercices/module1/1.1-1.2/src/components/Total/Total.jsx
+++ exercices/module1/1.1-1.2/src/components/Total/Total.jsx
@@ -1,9 +1,9 @@
 const Total = (props) => {
   return (
-    <p>
+    <div>
       Number of exercises{" "}
       {props.exercises1 + props.exercises2 + props.exercises3}
-    </p>
+    </div>
   );
 };
 
