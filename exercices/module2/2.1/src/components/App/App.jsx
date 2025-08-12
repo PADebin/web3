1bd65e9600dafc9f4a60916aab38c14936059bb4
PADebin <pierre.debin@student.vinci.be>
2025-08-12 18:04:06 +0200

    new:ex2.1-destructuring

--- exercices/module2/2.1/src/components/App/App.jsx
+++ exercices/module2/2.1/src/components/App/App.jsx
@@ -6,6 +6,8 @@ import ClickCounter from "components/ClickCounter/ClickCounter";
 
 function App() {
   const [count, setCount] = useState(0);
+  const message = "You are a master in the art of clicking !";
+  const title = "Mon compteur de clics";
 
   return (
     <>
@@ -18,7 +20,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <ClickCounter count={count} setCount={setCount} />
+      <ClickCounter
+        count={count}
+        setCount={setCount}
+        title={title}
+        message={message}
+      />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- exercices/module2/2.1/src/components/ClickCounter/ClickCounter.jsx
+++ exercices/module2/2.1/src/components/ClickCounter/ClickCounter.jsx
@@ -1,9 +1,11 @@
-function ClickCounter({ count, setCount }) {
+function ClickCounter({ count, setCount, title, message }) {
   return (
     <div className="card">
+      <h4>{title}</h4>
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
+      {count >= 10 && <p>{message}</p>}
       <p>
         Edit <code>src/App.jsx</code> and save to test HMR
       </p>
