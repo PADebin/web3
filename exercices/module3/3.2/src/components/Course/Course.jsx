67fe12dd07679727e1798e90e7077633df0544c5
PADebin <pierre.debin@student.vinci.be>
2025-08-13 15:53:39 +0200

    module3 : collection

--- exercices/module3/3.2/src/components/Course/Course.jsx
+++ exercices/module3/3.2/src/components/Course/Course.jsx
@@ -2,6 +2,12 @@ import Header from "../Header/Header";
 import Content from "../Content/Content";
 
 const Course = ({ course }) => {
+  /**
+   * Ou alors on peut utiliser :
+   * const { name: courseName, parts: courseParts } = course;
+   * et ensuite appeler seulement courseName et courseParts
+   * au lieu de course.name et course.parts
+   */
   return (
     <div>
       <Header course={course.name} />
