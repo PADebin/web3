30e3b8f81da085a44d01f1a8c061163e34d89433
PADebin <pierre.debin@student.vinci.be>
2025-08-13 17:12:27 +0200

    Module 4 : 4.1 - 2.9 SearchBar

--- exercices/module4/4.1/src/components/App/App.jsx
+++ exercices/module4/4.1/src/components/App/App.jsx
@@ -2,8 +2,15 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+  const [filter, setFilter] = useState("");
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
@@ -28,9 +35,23 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <div style={{ padding: "24px" }}>
       <h2>Phonebook</h2>
+      <div style={{ marginBottom: 16 }}>
+        <label htmlFor="filter-input">filter shown with: </label>
+        <input
+          id="filter-input"
+          value={filter}
+          onChange={handleFilterChange}
+          className="phonebook-input"
+        />
+      </div>
+      <h3>Add a new number</h3>
       <form onSubmit={handleSubmit}>
         <div style={{ marginBottom: 10 }}>
           <label htmlFor="name-input">name: </label>
@@ -58,7 +79,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {filteredPersons.map((person) => (
           <li key={person.name}>
             {person.name} {person.number && `: ${person.number}`}
           </li>
