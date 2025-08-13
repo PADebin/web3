0c6a514b10fa5ef91442916c0368939835a81777
PADebin <pierre.debin@student.vinci.be>
2025-08-13 12:18:05 +0200

    module3 : 1.6

---
+++ exercices/module3/3.1/src/components/App/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const App = () => {
+  // enregistrer les clics de chaque bouton dans un état différent
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div
+      style={{ maxWidth: 600, margin: "40px auto", fontFamily: "sans-serif" }}
+    >
+      <h1>Donnez votre avis</h1>
+      <div style={{ display: "flex", gap: 10, marginBottom: 20 }}>
+        <button onClick={() => setGood(good + 1)}>bon</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutre</button>
+        <button onClick={() => setBad(bad + 1)}>mauvais</button>
+      </div>
+      <h2>Statistiques</h2>
+      <div>bon : {good}</div>
+      <div>neutre : {neutral}</div>
+      <div>mauvais : {bad}</div>
+    </div>
+  );
+};
+
+export default App;
