fc7513e93aecab4671d151eb04644ec7517e4ca8
PADebin <pierre.debin@student.vinci.be>
2025-08-13 12:20:57 +0200

    module3 : 1.8

--- exercices/module3/3.1/src/components/App/App.jsx
+++ exercices/module3/3.1/src/components/App/App.jsx
@@ -1,5 +1,24 @@
 import { useState } from "react";
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const moyenne =
+    total === 0 ? 0 : ((good * 1 + neutral * 0 + bad * -1) / total).toFixed(2);
+  const percentPos =
+    total === 0 ? "0%" : ((good / total) * 100).toFixed(1) + "%";
+  return (
+    <div>
+      <div>bon : {good}</div>
+      <div>neutre : {neutral}</div>
+      <div>mauvais : {bad}</div>
+      <hr style={{ margin: "16px 0" }} />
+      <div>total : {total}</div>
+      <div>moyenne : {moyenne}</div>
+      <div>% positifs : {percentPos}</div>
+    </div>
+  );
+};
+
 const App = () => {
   // enregistrer les clics de chaque bouton dans un état différent
   const [good, setGood] = useState(0);
@@ -17,17 +36,7 @@ const App = () => {
         <button onClick={() => setBad(bad + 1)}>mauvais</button>
       </div>
       <h2>Statistiques</h2>
-      <div>bon : {good}</div>
-      <div>neutre : {neutral}</div>
-      <div>mauvais : {bad}</div>
-      <hr style={{margin: '16px 0'}} />
-      <div>total : {good + neutral + bad}</div>
-      <div>
-        moyenne : {good + neutral + bad === 0 ? 0 : ((good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)).toFixed(2)}
-      </div>
-      <div>
-        % positifs : {good + neutral + bad === 0 ? '0%' : ((good / (good + neutral + bad)) * 100).toFixed(1) + '%'}
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
