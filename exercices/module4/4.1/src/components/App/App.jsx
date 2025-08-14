e856daea3578db7704cb8193c99071d1dd248227
PADebin <pierre.debin@student.vinci.be>
2025-08-14 14:20:04 +0200

    Module 4 : 2.12

--- exercices/module4/4.1/src/components/App/App.jsx
+++ exercices/module4/4.1/src/components/App/App.jsx
@@ -26,9 +26,12 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    setPersons(persons.concat({ name: newName, number: newNumber }));
-    setNewName("");
-    setNewNumber("");
+    const newPerson = { name: newName, number: newNumber };
+    axios.post("http://localhost:3001/persons", newPerson).then((response) => {
+      setPersons(persons.concat(response.data));
+      setNewName("");
+      setNewNumber("");
+    });
   };
 
   const handleNameChange = (event) => {
