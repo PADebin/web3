0d1f21ef7fa977f30d4730d40debbfd5dfc7892b
PADebin <pierre.debin@student.vinci.be>
2025-08-14 14:23:56 +0200

    Module 4 : 2.13

---
+++ exercices/module4/4.1 copy/src/components/App/App.jsx
@@ -0,0 +1,68 @@
+import Filter from "components/Filter/Filter";
+import PersonForm from "components/Persons/PersonForm";
+import Persons from "components/Persons/Persons";
+import { useState, useEffect } from "react";
+import personsService from "../../services/persons";
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  useEffect(() => {
+    personsService.getAll().then((data) => {
+      setPersons(data);
+    });
+  }, []);
+  const [filter, setFilter] = useState("");
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (newName.trim() === "") return;
+    const nameExists = persons.some((person) => person.name === newName);
+    if (nameExists) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    const newPerson = { name: newName, number: newNumber };
+    personsService.create(newPerson).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson));
+      setNewName("");
+      setNewNumber("");
+    });
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div style={{ padding: "24px" }}>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} onChange={handleFilterChange} />
+      <h3>Add a new number</h3>
+      <PersonForm
+        onSubmit={handleSubmit}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={filteredPersons} />
+      {/* <div>debug: {newName}</div> */}
+    </div>
+  );
+};
+
+export default App;
