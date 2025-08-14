0777b0e17a1c665b6b2eb15376a9fe14151b9df3
PADebin <pierre.debin@student.vinci.be>
2025-08-14 14:22:12 +0200

    Module 4 : 2.13

--- exercices/module4/4.1/src/components/App/App.jsx
+++ exercices/module4/4.1/src/components/App/App.jsx
@@ -2,13 +2,13 @@ import Filter from "components/Filter/Filter";
 import PersonForm from "components/Persons/PersonForm";
 import Persons from "components/Persons/Persons";
 import { useState, useEffect } from "react";
-import axios from "axios";
+import personsService from "../../services/persons";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
+    personsService.getAll().then((data) => {
+      setPersons(data);
     });
   }, []);
   const [filter, setFilter] = useState("");
@@ -27,8 +27,8 @@ const App = () => {
       return;
     }
     const newPerson = { name: newName, number: newNumber };
-    axios.post("http://localhost:3001/persons", newPerson).then((response) => {
-      setPersons(persons.concat(response.data));
+    personsService.create(newPerson).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson));
       setNewName("");
       setNewNumber("");
     });
