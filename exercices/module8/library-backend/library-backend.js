5bf7f5ac464e058c9ea1c3f5a41a9273a7cc14a6
PADebin <pierre.debin@student.vinci.be>
2025-08-17 19:15:44 +0200

    Module8 - 8.3

--- exercices/module8/library-backend/library-backend.js
+++ exercices/module8/library-backend/library-backend.js
@@ -88,10 +88,18 @@ const typeDefs = `
     id: ID!
   }
 
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -100,6 +108,10 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => books.filter((b) => b.author === root.name).length,
   },
 };
 
